51768ae3b27e2f1efafceb03aa0ea9ed9f5b765e
ChenDong <1516216399@qq.com>
2024-03-14 18:02:25 +0800

    添加新的Servlet的功能为LifeServlet

--- src/main/java/org/example/_chendong2022211001000301/ChenDong/week3/demo/LiveServlet.java
+++ src/main/java/org/example/_chendong2022211001000301/ChenDong/week3/demo/LiveServlet.java
@@ -3,29 +3,28 @@ package org.example._chendong2022211001000301.ChenDong.week3.demo;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 
 public class LiveServlet extends HttpServlet {
-    public void doGet(HttpServletRequest request , HttpServletResponse response) {
-        PrintWriter writer;
-        try {
-            writer = response.getWriter();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        writer.println("Name : ChenDong");
-        writer.println("ID : 2022211001000301");
-        writer.println("Date and Time : Sunday, March 10, 2024,22:25:21");
+    public LiveServlet(){
+        System.out.println("in construction process");
     }
-    public void doPost(HttpServletRequest request , HttpServletResponse response) {
-        PrintWriter writer;
-        try {
-            writer = response.getWriter();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    @Override
+    public void init() {
+
+        System.out.print("in init");
+    }
+    @Override
+    protected void doGet(HttpServletRequest request , HttpServletResponse response) {
+        System.out.println("in servcie --> doget");
+    }
+    @Override
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) {
+        System.out.println("in servcie --> dopost");
+    }
+    public void destroy() {
+        System.out.println("in servcie --> destroy");
     }
 }
+
 
